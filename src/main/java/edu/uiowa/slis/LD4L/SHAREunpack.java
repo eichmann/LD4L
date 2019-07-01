7ac1349da2c871a5f1cbe0bf0aa75301b20b746e
eichmann <david-eichmann@uiowa.edu>
2019-07-01 19:19:23 +0000

    Working on new approach to robust loading of ShareVDE data.

--- src/main/java/edu/uiowa/slis/LD4L/SHAREunpack.java
+++ src/main/java/edu/uiowa/slis/LD4L/SHAREunpack.java
@@ -1,12 +1,14 @@
 package edu.uiowa.slis.LD4L;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,6 +19,14 @@ import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.compressors.CompressorException;
 import org.apache.commons.compress.compressors.CompressorInputStream;
 import org.apache.commons.compress.compressors.CompressorStreamFactory;
+import org.apache.jena.assembler.exceptions.UnknownEncodingException;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFFormat;
+import org.apache.jena.riot.RiotException;
+import org.apache.jena.sparql.core.Quad;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
@@ -24,6 +34,48 @@ public class SHAREunpack {
 	static Logger logger = Logger.getLogger(SHAREunpack.class);
 
     public static void main(String[] args) throws CompressorException, ArchiveException, IOException {
+	PropertyConfigurator.configure(args[0]);
+	File request = new File("/Users/eichmann/downloads/ckb");
+	if (request.isDirectory()) {
+	    for (File file : request.listFiles()) {
+		processFile(file);
+	    }
+	} else {
+	    processFile(request);
+	}
+    }
+    
+    static void processFile(File file) throws IOException, CompressorException, ArchiveException {
+	logger.info("processing " + file.getName() + "...");
+	File target = file.getName().endsWith("tgz") ? new File(file.getParent() + "/" + file.getName().replace(".tgz", ".nq"))
+		: new File(file.getParent() + "/" + file.getName().replace(".tar.gz", ".nq"));
+//	FileWriter writer = new FileWriter(target);
+	InputStream is = new FileInputStream(file);
+	CompressorInputStream in = new CompressorStreamFactory().createCompressorInputStream("gz", is);
+
+	ArchiveInputStream tin = new ArchiveStreamFactory().createArchiveInputStream("tar", in);
+	TarArchiveEntry entry = (TarArchiveEntry) tin.getNextEntry();
+	while (entry != null) {
+	    BufferedReader reader = new BufferedReader(new InputStreamReader((InputStream) tin));
+	    String buffer = null;
+	    Model model = ModelFactory.createDefaultModel();
+	    while ((buffer = reader.readLine()) != null) {
+		// writer.write(rewrite(buffer) + "\n");
+		logger.debug("\tquad: " + buffer);
+		try {
+		    RDFDataMgr.read(model,new ByteArrayInputStream(buffer.getBytes()), RDFFormat.NQUADS.getLang());
+		} catch (RiotException e) {
+		    logger.error("entry: " + entry.getName());
+		    logger.error("error parsing: " + buffer);
+		}
+	    }
+	    entry = (TarArchiveEntry) tin.getNextEntry();
+	}
+	is.close();
+//	writer.close();
+    }
+	
+     static void rewriteMain(String[] args) throws CompressorException, ArchiveException, IOException {
 	PropertyConfigurator.configure(args[0]);
 	File folder = new File("/Volumes/Pegasus3/LD4L/vde/cornell");
 //	File folder = new File("/Users/eichmann/downloads/cornell");
